ca7e32c881e5004706381bb61e40e9feae517967
Phil Lembo <phil.lembo@gmail.com>
2021-01-25 14:47:49 -0500

    Change to ignore click if winner found

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -20,6 +20,9 @@ class Board extends React.Component {
     }
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -44,7 +47,7 @@ class Board extends React.Component {
         } else {
             status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
-        
+
         return (
             <div>
                 <div className="status">{status}</div>
